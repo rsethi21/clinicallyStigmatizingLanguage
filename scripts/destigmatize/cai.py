00d5c08f4615505c63a568c54c060a83a6d08cdf
Rohan Sethi <rsethi1@luc.edu>
2025-06-04 09:22:01 -0500

    scaffolding for consitutional ai

---
+++ scripts/destigmatize/cai.py
@@ -0,0 +1,30 @@
+# imports non-AI
+
+# imports AI-specific
+
+# define arguments
+
+# read yaml
+
+# formatting prompt function
+
+# generation function
+
+# running on command line
+
+## instantiate arguments, data, hyperparameters
+
+## load models
+
+## data loop
+## provide current clinical note (probably those considered stigmatizing) as "initial response" and request critique using the constitution
+
+## provide critique to develop revision
+
+## repeat top two steps n times
+
+## use initial and final response for SFT
+
+## provide revisions for DPO
+
+## save model and data
